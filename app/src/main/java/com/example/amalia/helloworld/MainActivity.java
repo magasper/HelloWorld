b64065fd0bd44c4dc906e1ef1adbfdafbaa236a4
agastelumpc <agastelumpc@corporativosade.com.mx>
2017-09-07 13:12:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/amalia/helloworld/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.amalia.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void Test(){
+        int i = 1+1;
+    }
+}
